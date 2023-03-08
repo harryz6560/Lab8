f51e16a10a350f9b292275d9f2b1d170f04f1b77
Harry Zhao <wpzhao@ualberta.ca>
2023-03-07 17:41:06 -0700

    new change

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,6 +11,14 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> cityList = new ArrayList<City>();
 
+    @Test
+    public void testAddCity(){
+        CustomList list = new CustomList(null, cityList);
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 
 }
